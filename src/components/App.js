d8a026f0fe9db314e580c9500c594ec6df71f485
Jerusha <62611035+CelestialJerusha@users.noreply.github.com>
2023-11-08 21:52:28 +0000

    c

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,25 @@
 {/* <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p> */}
 
 import React from "react";
-// import ReactDOM from "react-dom";
-
-// const App = () =>{
-//     const root = ReactDOM.createRoot(
-//       document.getElementById('root')
-//     );
-//     const element = <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>;
+import ReactDOM from "react-dom";
+const root = ReactDOM.createRoot(
+          document.getElementById('root')
+        );
+const App = () =>{
+    
+    const element = <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>;
           
       
-//       root.render(element);
-//   }
-  
-//   root.render(element);
+      root.render(element);
+  }
 
-const App = () =>{
-    return(
-        <div id="main">
-            <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
-        </div>
-    )
-}
+
+// const App = () =>{
+//     return(
+//         <div id="main">
+//             <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+//         </div>
+//     )
+// }
 
 export default App;
